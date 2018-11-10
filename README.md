db1cf49373956b6d91ccf5542e60e94020c16fd2
ZiheLiu <272910663@qq.com>
2018-11-10 13:39:17 +0800

    Use mak-yek

--- README.md
+++ README.md
@@ -6,28 +6,70 @@
 
 ```c
 /**
- * 你的代码开始的地方
+ * YOUR CODE BEGIN
+ * 你的代码开始
  */
 
-void AiStart() {
+/**
+ * You can define your own struct and variable here
+ * 你可以在这里定义你自己的结构体和变量
+ */
+
+
+/**
+ * 你可以在这里初始化你的AI
+ */
+void initAI(int me) {
 
 }
 
-/**
- * 写入own_tanks_total个指令到commands中.
- */
-void AiRound() {
-  for (int i = 0; i < own_tanks_total; i++) {
-    commands[i] = MOVE;
+struct Command findValidPos(const char board[BOARD_SIZE][BOARD_SIZE], int flag) {
+  struct Command command = {0, 0, 0};
+  for (int k = 0; k < 4; k++) {
+    const int* delta = DIR[k];
+    for (int x = 0; x < BOARD_SIZE; x++) {
+      for (int y = 0; y < BOARD_SIZE; y++) {
+        if (board[x][y] != flag) {
+          continue;
+        }
+        int new_x = x + delta[0];
+        int new_y = y + delta[1];
+        if (is_in_bound(new_x, new_y) && board[new_x][new_y] == EMPTY) {
+          command.x = x;
+          command.y = y;
+          command.option = k;
+          return command;
+        }
+      }
+    }
   }
-}
-
-void AiEnd() {
-
+  return command;
 }
 
 /**
- * 你的代码结束的地方
+ * 轮到你落子。
+ * 棋盘上0表示空白，1表示黑棋，2表示白旗
+ * me表示你所代表的棋子(1或2)
+ * 你需要返回一个结构体Command，在x属性和y属性填上你想要移动的棋子的位置，option填上你想要移动的方向。
+ */
+struct Command aiTurn(const char board[BOARD_SIZE][BOARD_SIZE], int me) {
+  /*
+   * TODO：在这里写下你的AI。
+   * 这里有一个示例AI，它只会寻找第一个可下的位置进行落子。
+   */
+  struct Command preferedPos = findValidPos(board, me);
+
+  return preferedPos;
+}
+
+/**
+ * 你的代码结束
  */
 ```
 
+### 规则
+
+- 初始化：棋盘为8 * 8，黑白两方的棋子各16个，各占据上下的第一行和第三行。
+- 每回合：
+  - 黑棋先行
+  - 如果走完这一步，造成夹或者挑，把对应的敌方棋子变为我方棋子。
--- main.c
+++ main.c
@@ -2,153 +2,248 @@
 #include <stdlib.h>
 #include <string.h>
 
+// board information
+#define BOARD_SIZE 8
+#define EMPTY 0
+#define BLACK 1
+#define WHITE 2
+
+// bool
+typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
-// board
-#define TANK1_UP 'w'
-#define TANK1_DOWN 's'
-#define TANK1_LEFT 'a'
-#define TANK1_RIGHT 'd'
+// option
+typedef int OPTION;
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
 
-#define BULLET1_UP 'u'
-#define BULLET1_DOWN 'k'
-#define BULLET1_LEFT 'j'
-#define BULLET1_RIGHT 'l'
+#define MAX_BYTE 10000
 
-#define TANK2_UP 'W'
-#define TANK2_DOWN 'S'
-#define TANK2_LEFT 'A'
-#define TANK2_RIGHT 'D'
-
-#define BULLET2_UP 'U'
-#define BULLET2_DOWN 'K'
-#define BULLET2_LEFT 'J'
-#define BULLET2_RIGHT 'L'
-
-#define EMPTY '.'
-#define OBSTACLE '|'
-#define GRASS '?'
-#define FOOD1 '1'
-#define FOOD9 '9'
-
-// command
-#define TURN_TO_UP  0
-#define TURN_TO_DOWN 1
-#define TURN_TO_LEFT 2
-#define TURN_TO_RIGHT 3
-#define MOVE 4
-#define STOP 5
-#define SHOT 6
-
-// flags for two players
-#define PLAYER_FLAG_1 0
-#define PLAYER_FLAG_2 1
-
-// OPTION
 #define START "START"
-#define ROUND "ROUND"
-#define END "END"
+#define PLACE "PLACE"
+#define TURN  "TURN"
+#define END   "END"
 
-// score
-#define SCORE_KILL 4
-#define SCORE_FOOD1 1
-#define SCORE FOOD9 9
-
-#define MAX_BOARD_SIZE 100
-#define MAX_TANKS_TOTAL 10
-
-struct Position {
+struct Command
+{
   int x;
   int y;
+  OPTION option;
 };
 
-int own_flag;
-int board_size;
-int bullet_v;
-char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
-int own_tanks_total;
-struct Position own_tank_positions[MAX_TANKS_TOTAL];
-int commands[MAX_TANKS_TOTAL];
+char buffer[MAX_BYTE] = {0};
+char board[BOARD_SIZE][BOARD_SIZE] = {0};
+int me_flag;
+int other_flag;
 
+const int DIR[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
 
-/**
- * 你的代码开始的地方
- */
-
-void AiStart() {
-
+void debug(const char *str) {
+  printf("DEBUG %s\n", str);
+  fflush(stdout);
 }
 
-/**
- * 写入own_tanks_total个指令到commands中.
- */
-void AiRound() {
-  for (int i = 0; i < own_tanks_total; i++) {
-    commands[i] = MOVE;
+void print_board() {
+  char visual_board[BOARD_SIZE][BOARD_SIZE] = {0};
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+      if (board[i][j] == EMPTY) {
+        visual_board[i][j] = '.';
+      } else if (board[i][j] == BLACK) {
+        visual_board[i][j] = 'O';
+      } else if (board[i][j] == WHITE) {
+        visual_board[i][j] = 'X';
+      }
+    }
+    printf("%s\n", visual_board[i]);
   }
 }
 
-void AiEnd() {
-
+BOOL is_in_bound(int x, int y) {
+  return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
 }
 
 /**
- * 你的代码结束的地方
+ * YOUR CODE BEGIN
+ * 你的代码开始
  */
 
-void Start() {
-  scanf("%d%d%d", &board_size, &own_flag, &bullet_v);
+/**
+ * You can define your own struct and variable here
+ * 你可以在这里定义你自己的结构体和变量
+ */
+
+
+/**
+ * 你可以在这里初始化你的AI
+ */
+void initAI(int me) {
+
 }
 
-int IsOwnTank(char chr) {
-  return (own_flag == PLAYER_FLAG_1 &&
-          (chr == TANK1_UP || chr == TANK1_DOWN || chr == TANK1_LEFT || chr == TANK1_RIGHT)) ||
-         (own_flag == PLAYER_FLAG_2 &&
-          (chr == TANK2_UP || chr == TANK2_DOWN || chr == TANK2_LEFT || chr == TANK2_RIGHT));
-}
-
-void RoundStart() {
-  own_tanks_total = 0;
-  for (int i = 0; i < board_size; i++) {
-    scanf("%s", board[i]);
-
-    for (int j = 0; j < board_size; j++) {
-      if (IsOwnTank(board[i][j])) {
-        own_tanks_total++;
-        own_tank_positions[i].x = i;
-        own_tank_positions[i].y = j;
+struct Command findValidPos(const char board[BOARD_SIZE][BOARD_SIZE], int flag) {
+  struct Command command = {0, 0, 0};
+  for (int k = 0; k < 4; k++) {
+    const int* delta = DIR[k];
+    for (int x = 0; x < BOARD_SIZE; x++) {
+      for (int y = 0; y < BOARD_SIZE; y++) {
+        if (board[x][y] != flag) {
+          continue;
+        }
+        int new_x = x + delta[0];
+        int new_y = y + delta[1];
+        if (is_in_bound(new_x, new_y) && board[new_x][new_y] == EMPTY) {
+          command.x = x;
+          command.y = y;
+          command.option = k;
+          return command;
+        }
       }
     }
   }
+  return command;
 }
 
-void RoundEnd() {
-  for (int i = 0; i < own_tanks_total; i++) {
-    printf("%d%c", commands[i], i == own_tanks_total - 1 ? '\n' : ' ');
+/**
+ * 轮到你落子。
+ * 棋盘上0表示空白，1表示黑棋，2表示白旗
+ * me表示你所代表的棋子(1或2)
+ * 你需要返回一个结构体Command，在x属性和y属性填上你想要移动的棋子的位置，option填上你想要移动的方向。
+ */
+struct Command aiTurn(const char board[BOARD_SIZE][BOARD_SIZE], int me) {
+  /*
+   * TODO：在这里写下你的AI。
+   * 这里有一个示例AI，它只会寻找第一个可下的位置进行落子。
+   */
+  struct Command preferedPos = findValidPos(board, me);
+
+  return preferedPos;
+}
+
+/**
+ * 你的代码结束
+ */
+
+
+BOOL place(int x, int y, OPTION option, int cur_flag) {
+  // 移动之前的位置没有我方棋子
+  if (board[x][y] != cur_flag) {
+    return FALSE;
   }
-}
 
-void End() {
-}
+  int new_x = x + DIR[option][0];
+  int new_y = y + DIR[option][1];
+  // 移动之后的位置超出边界, 或者不是空地
+  if (!is_in_bound(new_x, new_y)) {
+    return FALSE;
+  }
 
-int main() {
-  char option[10];
+  board[x][y] = EMPTY;
+  board[new_x][new_y] = cur_flag;
 
-  while (TRUE) {
-    scanf("%s", option);
-    if (strstr(option, START)) {
-      Start();
-      printf("OK\n");
-    } else if (strstr(option, ROUND)) {
-      RoundStart();
-      AiRound();
-      RoundEnd();
-    } else if (strstr(option, END)) {
-      End();
-      AiEnd();
+  int cur_other_flag = 3 - cur_flag;
+
+  // 挑
+  int intervention_dir[2][2] = {{1, 0}, {0, 1}};
+  for (int i = 0; i < 2; i++) {
+    int x1 = new_x + intervention_dir[i][0];
+    int y1 = new_y + intervention_dir[i][1];
+    int x2 = new_x - intervention_dir[i][0];
+    int y2 = new_y - intervention_dir[i][1];
+    if (is_in_bound(x1, y1) && is_in_bound(x2, y2) && board[x1][y1] == cur_other_flag && board[x2][y] == cur_other_flag) {
+      board[x1][y1] = cur_flag;
+      board[x2][y2] = cur_flag;
     }
   }
 
+  // 夹
+  int custodian_dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, 2}};
+  for (int i = 0; i < 4; i++) {
+    int x1 = new_x + custodian_dir[i][0];
+    int y1 = new_y + custodian_dir[i][1];
+    int x2 = new_x + custodian_dir[i][0] * 2;
+    int y2 = new_y + custodian_dir[i][1] * 2;
+    if (is_in_bound(x1, y1) && is_in_bound(x2, y2) && board[x2][y2] == cur_flag && board[x1][y1] == cur_other_flag) {
+      board[x1][y1] = cur_flag;
+    }
+  }
+
+  return TRUE;
+}
+
+void start(int flag) {
+  memset(board, 0, sizeof(board));
+
+  char* first_row = board[0];
+  char* third_row = board[2];
+  char* bottom_first_row = board[BOARD_SIZE - 1];
+  char* bottom_third_tow = board[BOARD_SIZE - 3];
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    first_row[i] = WHITE;
+    third_row[i] = WHITE;
+    bottom_first_row[i] = BLACK;
+    bottom_third_tow[i] = BLACK;
+  }
+
+  initAI(flag);
+}
+
+void turn() {
+  // AI
+  struct Command command = aiTurn((const char (*)[BOARD_SIZE])board, me_flag);
+  place(command.x, command.y, command.option, me_flag);
+  printf("%d %d %d\n", command.x, command.y, command.option);
+  fflush(stdout);
+}
+
+void end(int x) {
+
+}
+
+void loop() {
+  freopen("../input", "r", stdin);
+  while (TRUE)
+  {
+    memset(buffer, 0, sizeof(buffer));
+    gets(buffer);
+
+    if (strstr(buffer, START))
+    {
+      char tmp[MAX_BYTE] = {0};
+      sscanf(buffer, "%s %d", tmp, &me_flag);
+      other_flag = 3 - me_flag;
+      start(me_flag);
+      printf("OK\n");
+      fflush(stdout);
+    }
+    else if (strstr(buffer, PLACE))
+    {
+      char tmp[MAX_BYTE] = {0};
+      int x, y;
+      OPTION option;
+      sscanf(buffer, "%s %d %d %d", tmp, &x, &y, &option);
+      place(x, y, option, other_flag);
+    }
+    else if (strstr(buffer, TURN))
+    {
+      turn();
+    }
+    else if (strstr(buffer, END))
+    {
+      char tmp[MAX_BYTE] = {0};
+      int x;
+      sscanf(buffer, "%s %d", tmp, &x);
+      end(x);
+    }
+    print_board();
+  }
+}
+
+
+int main(int argc, char *argv[]) {
+  loop();
   return 0;
 }
