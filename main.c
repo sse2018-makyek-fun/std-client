e894013efeaa9cf59f3ac6c20c0e001d7d371fff
tjsse519 <sse519@lab.com>
2019-11-18 00:50:10 +0800

    feat: implement printboard 2019

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 // board information
-#define BOARD_SIZE 12
+#define BOARD_SIZE 8
 #define EMPTY 0
 #define BLACK 1
 #define WHITE 2
@@ -15,14 +15,14 @@ typedef int BOOL;
 
 // option
 typedef int OPTION;
-#define UP 0
-#define DOWN 1
-#define LEFT 2
-#define RIGHT 3
-#define UP_LEFT 4
-#define UP_RIGHT 5
-#define DOWN_LEFT 6
-#define DOWN_RIGHT 7
+#define UP_LEFT 0
+#define UP_RIGHT 1
+#define DOWN_LEFT 2
+#define DOWN_RIGHT 3
+#define JUMP_UL 4
+#define JUMP_UR 5
+#define JUMP_DL 6
+#define JUMP_DR 7
 
 #define MAX_BYTE 10000
 
@@ -33,9 +33,9 @@ typedef int OPTION;
 
 struct Command
 {
-  int x;
-  int y;
-  OPTION option;
+    int x;
+    int y;
+    OPTION option;
 };
 
 char buffer[MAX_BYTE] = {0};
@@ -43,31 +43,31 @@ char board[BOARD_SIZE][BOARD_SIZE] = {0};
 int me_flag;
 int other_flag;
 
-const int DIR[8][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
+const int DIR[8][2] = { {-1, -1}, {-1, 1}, {1, -1}, {1, 1}, {-2, -2}, {-2, 2}, {2, -2}, {2, 2} };
 
 void debug(const char *str) {
-  printf("DEBUG %s\n", str);
-  fflush(stdout);
+    printf("DEBUG %s\n", str);
+    fflush(stdout);
 }
 
 void printBoard() {
-  char visual_board[BOARD_SIZE][BOARD_SIZE] = {0};
-  for (int i = 0; i < BOARD_SIZE; i++) {
-    for (int j = 0; j < BOARD_SIZE; j++) {
-      if (board[i][j] == EMPTY) {
-        visual_board[i][j] = '.';
-      } else if (board[i][j] == BLACK) {
-        visual_board[i][j] = 'O';
-      } else if (board[i][j] == WHITE) {
-        visual_board[i][j] = 'X';
-      }
+    char visual_board[BOARD_SIZE][BOARD_SIZE] = {0};
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j] == EMPTY) {
+                visual_board[i][j] = '.';
+            } else if (board[i][j] == BLACK) {
+                visual_board[i][j] = 'O';
+            } else if (board[i][j] == WHITE) {
+                visual_board[i][j] = 'X';
+            }
+        }
+        printf("%s\n", visual_board[i]);
     }
-    printf("%s\n", visual_board[i]);
-  }
 }
 
 BOOL isInBound(int x, int y) {
-  return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
+    return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
 }
 
 /**
@@ -85,30 +85,30 @@ BOOL isInBound(int x, int y) {
  * 你可以在这里初始化你的AI
  */
 void initAI(int me) {
-
+    
 }
 
 struct Command findValidPos(const char board[BOARD_SIZE][BOARD_SIZE], int flag) {
-  struct Command command = {0, 0, 0};
-  for (int k = 0; k < 8; k++) {
-    const int* delta = DIR[k];
-    for (int x = 0; x < BOARD_SIZE; x++) {
-      for (int y = 0; y < BOARD_SIZE; y++) {
-        if (board[x][y] != flag) {
-          continue;
+    struct Command command = {0, 0, 0};
+    for (int k = 0; k < 8; k++) {
+        const int* delta = DIR[k];
+        for (int x = 0; x < BOARD_SIZE; x++) {
+            for (int y = 0; y < BOARD_SIZE; y++) {
+                if (board[x][y] != flag) {
+                    continue;
+                }
+                int new_x = x + delta[0];
+                int new_y = y + delta[1];
+                if (isInBound(new_x, new_y) && board[new_x][new_y] == EMPTY) {
+                    command.x = x;
+                    command.y = y;
+                    command.option = k;
+                    return command;
+                }
+            }
         }
-        int new_x = x + delta[0];
-        int new_y = y + delta[1];
-        if (isInBound(new_x, new_y) && board[new_x][new_y] == EMPTY) {
-          command.x = x;
-          command.y = y;
-          command.option = k;
-          return command;
-        }
-      }
     }
-  }
-  return command;
+    return command;
 }
 
 /**
@@ -118,13 +118,13 @@ struct Command findValidPos(const char board[BOARD_SIZE][BOARD_SIZE], int flag) 
  * 你需要返回一个结构体Command，在x属性和y属性填上你想要移动的棋子的位置，option填上你想要移动的方向。
  */
 struct Command aiTurn(const char board[BOARD_SIZE][BOARD_SIZE], int me) {
-  /*
-   * TODO：在这里写下你的AI。
-   * 这里有一个示例AI，它只会寻找第一个可下的位置进行落子。
-   */
-  struct Command preferedPos = findValidPos(board, me);
-
-  return preferedPos;
+    /*
+     * TODO：在这里写下你的AI。
+     * 这里有一个示例AI，它只会寻找第一个可下的位置进行落子。
+     */
+    struct Command preferedPos = findValidPos(board, me);
+    
+    return preferedPos;
 }
 
 /**
@@ -133,131 +133,129 @@ struct Command aiTurn(const char board[BOARD_SIZE][BOARD_SIZE], int me) {
 
 
 BOOL place(int x, int y, OPTION option, int cur_flag) {
-  // 移动之前的位置没有我方棋子
-  if (board[x][y] != cur_flag) {
-    return FALSE;
-  }
-
-  int new_x = x + DIR[option][0];
-  int new_y = y + DIR[option][1];
-  // 移动之后的位置超出边界, 或者不是空地
-  if (!isInBound(new_x, new_y) || board[new_x][new_y] != EMPTY) {
-    return FALSE;
-  }
-
-  board[x][y] = EMPTY;
-  board[new_x][new_y] = cur_flag;
-
-  int cur_other_flag = 3 - cur_flag;
-
-  // 挑
-  int intervention_dir[4][2] = { {1, 0}, {0, 1}, {1, 1}, {1, -1} };
-  for (int i = 0; i < 4; i++) {
-    int x1 = new_x + intervention_dir[i][0];
-    int y1 = new_y + intervention_dir[i][1];
-    int x2 = new_x - intervention_dir[i][0];
-    int y2 = new_y - intervention_dir[i][1];
-    if (isInBound(x1, y1) && isInBound(x2, y2) && board[x1][y1] == cur_other_flag && board[x2][y2] == cur_other_flag) {
-      board[x1][y1] = cur_flag;
-      board[x2][y2] = cur_flag;
+    // 移动之前的位置没有我方棋子
+    if (board[x][y] != cur_flag) {
+        return FALSE;
     }
-  }
-
-  // 夹
-  int custodian_dir[8][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
-  for (int i = 0; i < 8; i++) {
-    int x1 = new_x + custodian_dir[i][0];
-    int y1 = new_y + custodian_dir[i][1];
-    int x2 = new_x + custodian_dir[i][0] * 2;
-    int y2 = new_y + custodian_dir[i][1] * 2;
-    if (isInBound(x1, y1) && isInBound(x2, y2) && board[x2][y2] == cur_flag && board[x1][y1] == cur_other_flag) {
-      board[x1][y1] = cur_flag;
+    
+    int new_x = x + DIR[option][0];
+    int new_y = y + DIR[option][1];
+    // 移动之后的位置超出边界, 或者不是空地
+    if (!isInBound(new_x, new_y) || board[new_x][new_y] != EMPTY) {
+        return FALSE;
     }
-  }
-
-  return TRUE;
+    
+    board[x][y] = EMPTY;
+    board[new_x][new_y] = cur_flag;
+    
+    int cur_other_flag = 3 - cur_flag;
+    
+    // 挑
+    int intervention_dir[4][2] = { {1, 0}, {0, 1}, {1, 1}, {1, -1} };
+    for (int i = 0; i < 4; i++) {
+        int x1 = new_x + intervention_dir[i][0];
+        int y1 = new_y + intervention_dir[i][1];
+        int x2 = new_x - intervention_dir[i][0];
+        int y2 = new_y - intervention_dir[i][1];
+        if (isInBound(x1, y1) && isInBound(x2, y2) && board[x1][y1] == cur_other_flag && board[x2][y2] == cur_other_flag) {
+            board[x1][y1] = cur_flag;
+            board[x2][y2] = cur_flag;
+        }
+    }
+    
+    // 夹
+    int custodian_dir[8][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+    for (int i = 0; i < 8; i++) {
+        int x1 = new_x + custodian_dir[i][0];
+        int y1 = new_y + custodian_dir[i][1];
+        int x2 = new_x + custodian_dir[i][0] * 2;
+        int y2 = new_y + custodian_dir[i][1] * 2;
+        if (isInBound(x1, y1) && isInBound(x2, y2) && board[x2][y2] == cur_flag && board[x1][y1] == cur_other_flag) {
+            board[x1][y1] = cur_flag;
+        }
+    }
+    
+    return TRUE;
 }
 
 
+//.X.X.X.X
+//X.X.X.X.
+//.X.X.X.X
 //........
 //........
-//..XXXX..
-//XXXX....
-//....OOOO
-//..OOOO..
-//........
-//........
+//O.O.O.O.
+//.O.O.O.O
+//O.O.O.O.
 void start(int flag) {
-  memset(board, 0, sizeof(board));
-
-  for (int i = 0; i < 3; i++) {
-    board[2][2 + i] = WHITE;
-    board[6][6 + i] = WHITE;
-    board[5][3 + i] = BLACK;
-    board[9][7 + i] = BLACK;
-  }
-
-  for (int i = 0; i < 2; i++) {
-    board[8 + i][2] = WHITE;
-    board[2 + i][9] = BLACK;
-  }
-
-  initAI(flag);
+    memset(board, 0, sizeof(board));
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 8; j += 2)
+        {
+            board[i][j + (i + 1) % 2] = WHITE;
+        }
+    }
+    for (int  i = 5; i < 8; i++) {
+        for (int j = 0; j < 8; j += 2)
+        {
+            board[i][j + (i + 1) % 2] = BLACK;
+        }
+    }
+    
+    initAI(flag);
 }
 
 void turn() {
-  // AI
-  struct Command command = aiTurn((const char (*)[BOARD_SIZE])board, me_flag);
-  place(command.x, command.y, command.option, me_flag);
-  printf("%d %d %d\n", command.x, command.y, command.option);
-  fflush(stdout);
+    // AI
+    struct Command command = aiTurn((const char (*)[BOARD_SIZE])board, me_flag);
+    place(command.x, command.y, command.option, me_flag);
+    printf("%d %d %d\n", command.x, command.y, command.option);
+    fflush(stdout);
 }
 
 void end(int x) {
-
+    
 }
 
 void loop() {
-//  freopen("../input", "r", stdin);
-  while (TRUE)
-  {
-    memset(buffer, 0, sizeof(buffer));
-    gets(buffer);
-
-    if (strstr(buffer, START))
+    //  freopen("../input", "r", stdin);
+    char tmp[MAX_BYTE] = {0};
+    int x, y;
+    OPTION option;
+    while (TRUE)
     {
-      char tmp[MAX_BYTE] = {0};
-      sscanf(buffer, "%s %d", tmp, &me_flag);
-      other_flag = 3 - me_flag;
-      start(me_flag);
-      printf("OK\n");
-      fflush(stdout);
+        memset(buffer, 0, sizeof(buffer));
+        gets(buffer);
+        memset(tmp, 0, sizeof(tmp));
+        if (strstr(buffer, START))
+        {
+            sscanf(buffer, "%s %d", tmp, &me_flag);
+            other_flag = 3 - me_flag;
+            start(me_flag);
+            printf("OK\n");
+            fflush(stdout);
+        }
+        else if (strstr(buffer, PLACE))
+        {
+            sscanf(buffer, "%s %d %d %d", tmp, &x, &y, &option);
+            place(x, y, option, other_flag);
+        }
+        else if (strstr(buffer, TURN))
+        {
+            turn();
+        }
+        else if (strstr(buffer, END))
+        {
+            sscanf(buffer, "%s %d", tmp, &x);
+            end(x);
+        }
+        //    printBoard();
     }
-    else if (strstr(buffer, PLACE))
-    {
-      char tmp[MAX_BYTE] = {0};
-      int x, y;
-      OPTION option;
-      sscanf(buffer, "%s %d %d %d", tmp, &x, &y, &option);
-      place(x, y, option, other_flag);
-    }
-    else if (strstr(buffer, TURN))
-    {
-      turn();
-    }
-    else if (strstr(buffer, END))
-    {
-      char tmp[MAX_BYTE] = {0};
-      int x;
-      sscanf(buffer, "%s %d", tmp, &x);
-      end(x);
-    }
-//    printBoard();
-  }
 }
 
 
 int main(int argc, char *argv[]) {
-  loop();
-  return 0;
+    loop();
+    return 0;
 }
