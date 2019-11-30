07948f70a9e8365a753d708617f037c67b6aca75
OneG <zhishou221@hotmail.com>
2019-11-30 23:18:11 +0800

    style: change to Camel-Case

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
-#define MAX_BYTE 10000
 #define MAX_STEP 15
 
 #define START "START"
@@ -29,8 +28,8 @@ struct Command
 };
 
 char board[BOARD_SIZE][BOARD_SIZE] = {0};
-int me_flag;
-int other_flag;
+int meFlag;
+int otherFlag;
 
 void debug(const char *str)
 {
@@ -40,25 +39,25 @@ void debug(const char *str)
 
 void printBoard()
 {
-    char visual_board[BOARD_SIZE][BOARD_SIZE + 1] = {0};
+    char visualBoard[BOARD_SIZE][BOARD_SIZE + 1] = {0};
     for (int i = 0; i < BOARD_SIZE; i++)
     {
         for (int j = 0; j < BOARD_SIZE; j++)
         {
             if (board[i][j] == EMPTY)
             {
-                visual_board[i][j] = '.';
+                visualBoard[i][j] = '.';
             }
             else if (board[i][j] == BLACK)
             {
-                visual_board[i][j] = 'O';
+                visualBoard[i][j] = 'O';
             }
             else if (board[i][j] == WHITE)
             {
-                visual_board[i][j] = 'X';
+                visualBoard[i][j] = 'X';
             }
         }
-        printf("%s\n", visual_board[i]);
+        printf("%s\n", visualBoard[i]);
     }
 }
 
@@ -67,18 +66,18 @@ BOOL isInBound(int x, int y)
     return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
 }
 
-void place(struct Command cmd, int cur_flag)
+void place(struct Command cmd, int curFlag)
 {
-    int x_mid, y_mid;
+    int xMid, yMid;
     for (int i = 0; i < cmd.numStep - 1; i++)
     {
         board[cmd.x[i]][cmd.y[i]] = EMPTY;
-        board[cmd.x[i + 1]][cmd.y[i + 1]] = cur_flag;
+        board[cmd.x[i + 1]][cmd.y[i + 1]] = curFlag;
         if (abs(cmd.x[i] - cmd.x[i + 1]) == 2)
         {
-            x_mid = (cmd.x[i] + cmd.x[i + 1]) / 2;
-            y_mid = (cmd.y[i] + cmd.y[i + 1]) / 2;
-            board[x_mid][y_mid] = EMPTY;
+            xMid = (cmd.x[i] + cmd.x[i + 1]) / 2;
+            yMid = (cmd.y[i] + cmd.y[i + 1]) / 2;
+            board[xMid][yMid] = EMPTY;
         }
     }
 }
@@ -155,8 +154,8 @@ void start(int flag)
 void turn()
 {
     // AI
-    struct Command command = aiTurn((const char(*)[BOARD_SIZE])board, me_flag);
-    place(command, me_flag);
+    struct Command command = aiTurn((const char(*)[BOARD_SIZE])board, meFlag);
+    place(command, meFlag);
     printf("%d", command.numStep);
     for (int i = 0; i < command.numStep; i++)
     {
@@ -172,7 +171,6 @@ void end(int x)
 
 void loop()
 {
-    //  freopen("../input", "r", stdin);
     char tag[10] = {0};
     struct Command command = {
         .x = {0},
@@ -185,9 +183,9 @@ void loop()
         scanf("%s", tag);
         if (strcmp(tag, START) == 0)
         {
-            scanf("%d", &me_flag);
-            other_flag = 3 - me_flag;
-            start(me_flag);
+            scanf("%d", &meFlag);
+            otherFlag = 3 - meFlag;
+            start(meFlag);
             printf("OK\n");
             fflush(stdout);
         }
@@ -197,7 +195,7 @@ void loop()
             for (int i = 0; i < command.numStep; i++)
             {
                 scanf("%d,%d", &command.x[i], &command.y[i]);
-                place(command, other_flag);
+                place(command, otherFlag);
             }
         }
         else if (strcmp(tag, TURN) == 0)
@@ -209,7 +207,6 @@ void loop()
             scanf("%d", &status);
             end(status);
         }
-        //    printBoard();
     }
 }
 
